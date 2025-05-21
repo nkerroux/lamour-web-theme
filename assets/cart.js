ba625d8fdbf8962541bf90fab85fdc06beb81414
Néo Kerroux <nkerroux@gmail.com>
2025-05-21 09:46:28 +0200

    logs

--- assets/cart.js
+++ assets/cart.js
@@ -45,15 +45,15 @@ class CartItems extends HTMLElement {
         ],
       };
 
-      config.body = JSON.stringify(formData);
-      fetch(`${routes.cart_add_url}`, config)
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      // config.body = JSON.stringify(formData);
+      // fetch(`${routes.cart_add_url}`, config)
+      //   .then((response) => response.json())
+      //   .then((response) => {
+      //     console.log(response);
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error:', error);
+      //   });
 
       fetch(`${routes.cart_add_url}`, {
         method: 'POST',
